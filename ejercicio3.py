476250d438695b20dd05dc5441395e095f6e8035
Alejan12 <alejandro.egidojimenez@usp.ceu.es>
2022-10-27 20:12:35 +0200

    subir nuevo

--- ejercicio3.py
+++ ejercicio3.py
@@ -3,6 +3,12 @@ class Alumno():
     def __init__(self,clase,notas):
         self.clase=clase
         self.notas=notas
+    def calificar(clase,notas):
+        for i in range(len(notas)):
+            if int(notas[i])<5:
+                print("El alumno "+clase[i]+" ha suspendido Algoritmos")
+            else:
+                print("El alumno "+clase[i]+" ha aprobado Algoritmos")
     def __str__(self):
         return "El alumno {} se ha creado con exito  ##".format(self.clase)
 clase=deque()
@@ -11,13 +17,7 @@ clase.append(input("Introduzca el nombre del alumo: "))
 clase.append(input("Introduzca el nombre del alumo: "))
 notas.append(input("Introduzca la nota del alumno: "))
 notas.append(input("Introduzca la nota del alumno: "))
-def calificar(clase,notas):
-    for i in range(len(notas)):
-        if int(notas[i])<5:
-            print("El alumno "+clase[i]+" ha suspendido Algoritmos")
-        else:
-            print("El alumno "+clase[i]+" ha aprobado Algoritmos")
 a=Alumno(clase[0],notas[0])
 b=Alumno(clase[1],notas[1])
 print(a,b)
-calificar(clase,notas)
+print(Alumno.calificar(clase,notas))
