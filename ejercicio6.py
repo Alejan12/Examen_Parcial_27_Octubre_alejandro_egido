aee2e9990f5ecf27f607dd09769f0f9dca443475
Alejan12 <alejandro.egidojimenez@usp.ceu.es>
2022-10-27 20:30:53 +0200

    subir nuevo

--- ejercicio6.py
+++ ejercicio6.py
@@ -1,7 +1,8 @@
-def matriz(a,b):
+def matriz(a,b):# te va a salir en horizontal, no me preguntes porque poque no lo entiendo, supongo que tu si que lo sabes
     matriz=[]
     for i in range(a):
         c=[7]*b
         matriz.append(c)
     return matriz
 print(matriz(5,5))
+
